430f81a748b3e2021f3f5be107feee3576dbbb48
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-11-06 15:42:29 +0700

    viet bao cao thuiiii yeee

--- WebsiteBanHang/Controllers/DonHangController.cs
+++ WebsiteBanHang/Controllers/DonHangController.cs
@@ -11,10 +11,17 @@ namespace WebsiteBanHang.Controllers
     {
         Entities db = new Entities();
         // GET: DonHang
-        public ActionResult Index(int? MaTV)
+
+        public ActionResult buttonDonHangPartial()
         {
-            
-           List<DonDatHang>  listdh = db.DonDatHangs.Where(n => n.MaKH == MaTV && n.MaTinhTrangGiaoHang == 5).ToList();
+
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            ThanhVien tv = Session["TaiKhoan"] as ThanhVien;
+            List<DonDatHang>  listdh = db.DonDatHangs.Where(n => n.MaKH == tv.MaThanhVien && n.MaTinhTrangGiaoHang == 5).OrderByDescending(p=>p.NgayDat).ToList();
+
 
             return View(listdh);
         }
@@ -33,10 +40,59 @@ namespace WebsiteBanHang.Controllers
 
 
         {
+
             ThanhVien tv = Session["TaiKhoan"] as ThanhVien;
             List<DonDatHang> listdh = db.DonDatHangs.Where(n => n.MaKH == tv.MaThanhVien && n.MaTinhTrangGiaoHang == 7).ToList();
 
             return View(listdh);
         }
+
+        public ActionResult XacNhanDonHang(string MaDDH)
+        {
+            DonDatHang dhh = db.DonDatHangs.SingleOrDefault(n => n.MaDDH.Equals(MaDDH));
+            if(dhh == null)
+            {
+                Response.StatusCode = 404;
+            }
+            else
+            {
+                dhh.MaTinhTrangGiaoHang = 7;
+                db.SaveChanges();
+            }
+            return RedirectToAction("DHDangVanChuyen", "DonHang");
+        }
+
+        public JsonResult HuyDonHang(string MaDDH)
+        {
+
+            if (MaDDH == null)
+            {
+                Response.StatusCode = 404;
+            }
+            else
+            {
+                List<ChiTietDonDatHang> listCT = db.ChiTietDonDatHangs.Where(n => n.MaDDH == MaDDH).ToList();
+                for (var i = 0; i < listCT.Count(); i++)
+                {
+                    db.ChiTietDonDatHangs.Remove(listCT[i]);
+                }
+                DonDatHang ddh = db.DonDatHangs.SingleOrDefault(n => n.MaDDH == MaDDH);
+                db.DonDatHangs.Remove(ddh);
+
+
+            }
+            db.SaveChanges();
+            return Json(new { status = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult XemChiTietDonHang(string MaDDH)
+        {
+
+            List<ChiTietDonDatHang> ctddh = db.ChiTietDonDatHangs.Where(n => n.MaDDH == MaDDH).ToList();
+            
+            return View(ctddh);
+        }
+
+
     }
 }
